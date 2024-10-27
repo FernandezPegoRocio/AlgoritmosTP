db2fc77e48b5c381ba2fa5a3c93bdbf1f613efa8
Rocío <162938513+FernandezPegoRocio@users.noreply.github.com>
2024-10-27 12:02:11 -0300

    Add files via upload

---
+++ Unidad9/unidad7Modularizado/include/Numeros.hpp
@@ -0,0 +1,12 @@
+// Numeros.hpp
+#ifndef NUMEROS_HPP
+#define NUMEROS_HPP
+
+namespace Numeros {
+    void llenarImpares(int* array, int& tam);
+    void llenarPrimos(int* array, int& tam);
+    void llenarEnteros(int* array, int tam);
+    void mostrarArray(const int* array, int tam);
+}
+
+#endif // NUMEROS_HPP
