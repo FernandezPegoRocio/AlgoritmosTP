b2c8ff528035784f9820730542d5d375d3c4afc2
Rocío <162938513+FernandezPegoRocio@users.noreply.github.com>
2024-11-05 00:31:01 -0300

    Add files via upload

---
+++ Unidad10/1_numerosPares/include/numerosPares.hpp
@@ -0,0 +1,12 @@
+#ifndef NUMEROSPARES_HPP
+#define NUMEROSPARES_HPP
+
+namespace Numeros {
+    class Pares {
+    public:
+        static void generar(int pares[], int &tamano);
+        static void mostrarPares(int *pares, int tamano);
+    };
+}
+
+#endif 
