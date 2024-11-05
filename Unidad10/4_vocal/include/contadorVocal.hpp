fa07a3fa2de24200a183ee4e9fc00d440e19ade7
Rocío <162938513+FernandezPegoRocio@users.noreply.github.com>
2024-11-05 10:11:52 -0300

    Add files via upload

---
+++ Unidad10/4_vocal/include/contadorVocal.hpp
@@ -0,0 +1,11 @@
+#ifndef CONTADORVOCALES_HPP
+#define CONTADORVOCALES_HPP
+
+namespace Contador {
+    class CadenaVocal {
+    public:
+        static void contarVocales(const char *cadena, int &a, int &e, int &i, int &o, int &u);
+    };
+}
+
+#endif 
