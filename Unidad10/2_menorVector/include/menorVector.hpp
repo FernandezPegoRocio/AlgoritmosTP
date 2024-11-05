1ac7a2f613a4bc14df15ae618b7e5a8c9c6423c8
Rocío <162938513+FernandezPegoRocio@users.noreply.github.com>
2024-11-05 10:07:17 -0300

    Add files via upload

---
+++ Unidad10/2_menorVector/include/menorVector.hpp
@@ -0,0 +1,12 @@
+#ifndef MENORVECTOR_HPP
+#define MENORVECTOR_HPP
+
+namespace Numeros {
+    class MenorVector {
+    public:
+        static void generar(int numeros[], int tamano);
+        static int encontrarMenor(int *numeros, int tamano);
+    };
+}
+
+#endif
